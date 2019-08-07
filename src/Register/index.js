752d04565f7a897f0c782fea257ce0058b7cbcba
Chris Roman <Christopherroman85@gmail.com>
2019-08-07 09:33:00 -0500

    refactored Register component

--- src/Register/index.js
+++ src/Register/index.js
@@ -56,44 +56,60 @@ class Register extends Component {
     render(){   
     	if(this.props.userInfo.loginFail === true) {
     		return (
-		      <div>
-                <h6>Looks like the credentials you entered are incorrect. Do you need to make an account?</h6>
-                    <div is="custom-elem" class='registerCard'>
-                        <div is="custom-elem" class='registerContainer'>
-                            <h1>Register</h1>
-                            <form onSubmit={this.handleSubmit}>
-                              <input name='username' placeholder='Username' type='text' onChange={this.handleChange}></input>
-                              <br/>
-                              <br/>
-                              <input name='password' placeholder='Password' type='password' onChange={this.handleChange}></input>
-                              <br/>
-                              <br/>
-                              <button is="custom-elem" class='register'>Submit</button>
-                            </form>
-                            <h6>Have an Account? <button className='register' onClick={this.props.haveAnAccount}>Close</button></h6>
+                <div className="container">
+                    <h6>Looks like the credentials you entered are incorrect. Do you need to make an account?</h6>
+                    <div className='row valign-wrapper center'>
+                        <div className='col s m7 l6 offset-m3 offset-l3 valign'>
+                            <div className='card blue-grey lighten-4'>
+                                <div className='card-action blue darken-2 white-text'>
+                                    <h2 className='blue-grey-text text-lighten-4'>Register</h2>
+                                </div>
+                                <div className='card-content'>
+                                    <div className='form-field'>
+                                        <label>Username</label>
+                                        <input type='text' name='username' onChange={this.handleChange}></input><br/>
+                                    </div>
+                                    <div className='form-field'>
+                                        <label>Password</label>
+                                        <input type='password' name='password' onChange={this.handleChange}></input><br/>
+                                    </div>
+                                    <div className='form-field center-align'>
+                                        <button onClick={this.handleSubmit} className="btn-large waves-effect waves-light blue darken-2 blue-grey-text text-lighten-4 center-align">Register</button>
+                                    </div>
+                                    <small><h6>Have an Account? <button className='btn-flat waves-effect waves-light btn-small' onClick={this.props.haveAnAccount}>Login</button></h6></small>
+                                </div>
+                            </div>  
                         </div>
                     </div>
                 </div>
 	    	)
     	} else if(this.props.userInfo.loginFail === false && this.props.userInfo.register === true) {
     		return (
-    			<div>
-                    <div is="custom-elem" class='registerCard'>
-                        <div is="custom-elem" class='registerContainer'>
-                            <h1>Register</h1>
-        	    			<form onSubmit={this.handleSubmit}>
-        			          <input name='username' placeholder='Username' type='text' onChange={this.handleChange}></input>
-        			          <br/>
-                              <br/>
-                              <input name='password' placeholder='Password' type='password' onChange={this.handleChange}></input>
-        			          <br/>
-                              <br/>
-                              <button is="custom-elem" class='register'>Submit</button>
-        			        </form>
-                            <h6>Have an Account? <button className='register' onClick={this.props.haveAnAccount}>Close</button></h6>
+                <div className="container">
+                    <div className='row valign-wrapper center'>
+                        <div className='col s m7 l6 offset-m3 offset-l3 valign'>
+                            <div className='card blue-grey lighten-4'>
+                                <div className='card-action blue darken-2 white-text'>
+                                    <h2 className='blue-grey-text text-lighten-4'>Register</h2>
+                                </div>
+                                <div className='card-content'>
+                                    <div className='form-field'>
+                                        <label>Username</label>
+                                        <input type='text' name='username' onChange={this.handleChange}></input><br/>
+                                    </div>
+                                    <div className='form-field'>
+                                        <label>Password</label>
+                                        <input type='password' name='password' onChange={this.handleChange}></input><br/>
+                                    </div>
+                                    <div className='form-field center-align'>
+                                        <button onClick={this.handleSubmit} className="btn-large waves-effect waves-light blue darken-2 blue-grey-text text-lighten-4 center-align">Register</button>
+                                    </div>
+                                    <small><h6>Have an Account? <button className='btn-flat waves-effect waves-light btn-small' onClick={this.props.haveAnAccount}>Login</button></h6></small>
+                                </div>
+                            </div>  
                         </div>
                     </div>
-			    </div>
+                </div>
     		)
     	} 
     }
